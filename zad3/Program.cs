f4b1e58351bf38cd4b92a455b3fd771d62b46de8
igor <desant350@gmail.com>
2022-07-13 16:18:08 +0200

    zad3

---
+++ zad3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+double[] array = { -34, -22, 16, 66, 9, 22, 65,};
+Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
+double differenceMaxMin = SearchMax(array) - SearchMin(array);
+Console.WriteLine($"Разница между max и min = {differenceMaxMin}");
+
+double SearchMax(double[] array)
+{
+    int i = 0;
+    double max = array[i];
+    for (i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    Console.WriteLine($"max = {max}");
+    return max;
+}
+
+double SearchMin(double[] array)
+{
+    int index = 0;
+    double min = array[index];
+    for (index = 1; index < array.Length; index++)
+    {
+        if (array[index] < min) min = array[index];
+    }
+    Console.WriteLine($"min = {min}");
+    return min;
+}
